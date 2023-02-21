5abaef4ab867f13d1a3b2b26237eb3eefa828c8a
--replace-all <samma118@hotmail.com>
2023-02-21 14:28:50 -0500

    Build button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,33 @@ import React from "react";
 
 import "components/Button.scss";
 
+// PROPS
+// children (rmb, cannot be renamed)
+// base
+// confirm
+// danger
+// clickable
+// disabled
+
 export default function Button(props) {
-   return <></>;
+ const { children, base, confirm, danger, onClick, disabled } = props;
+
+  let buttonClass = "button";
+
+  if (confirm) {
+    buttonClass += " button--confirm";
+  }
+  if (danger) {
+    buttonClass += " button--danger";
+  }
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 }
