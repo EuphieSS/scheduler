9b804b0c06d265c2aac5729683157921a7106d67
--replace-all <samma118@hotmail.com>
2023-02-22 17:05:35 -0500

    Set up basic structure for new component and story

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem"
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
 
 
 /////////////// BUTTON COMPONENT ///////////////
@@ -139,3 +140,17 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+  /////////////// APPOINTMENT COMPONENT ///////////////
+  storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => (
+      <Appointment />
+    ))
+    .add("Appointment with Time", () => (
+      <Appointment
+        time="12pm"
+      />
+    ));
