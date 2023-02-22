a2b9fed289c8734a64b905865935ca9c48d7ca34
--replace-all <samma118@hotmail.com>
2023-02-21 21:03:44 -0500

    Add component and mock data to test functionality

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,28 @@ import React from "react";
 
 import "components/Application.scss";
 
+import DayList from "./DayList";
+
+
+///////////// MOCK DATA /////////////
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 export default function Application(props) {
   return (
     <main className="layout">
@@ -11,14 +33,24 @@ export default function Application(props) {
           src="images/logo.png"
           alt="Interview Scheduler"
         />
+        
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        
+        <nav className="sidebar__menu">
+        <DayList
+          days={days}
+          day={"Monday"}
+          setDay={day => console.log(day)}
+        />
+        </nav>
+        
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
       </section>
+
       <section className="schedule">
         {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
       </section>
