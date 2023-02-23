2f35d4505430f8fae2ae29b5f7462ffe7c58413e
--replace-all <samma118@hotmail.com>
2023-02-22 23:52:48 -0500

    Use fragments and conditional rendering to display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,11 +2,25 @@ import React from "react";
 
 import "components/Appointment/styles.scss"
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 
 export default function Appointment(props) {
-  const { time } = props;
+  const { time, interview } = props;
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      {/** All <Appointment> components will render a <Header> that takes in a time prop */}
+      <Header time={time} />
+      {interview ?
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+        : <Empty />
+      }
+    </article>
   );
 }
